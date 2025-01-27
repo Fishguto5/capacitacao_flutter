26b0c5b06125a46ab084a7ecc84b92151b00fd7c
Gustavo Fernandes <gutof.carvalho@gmail.com>
2025-01-27 18:06:05 -0300

    Login Page Front-end is ready, only thing left is depencies of firebase and implent

--- capacitacao_flutter/lib/screens/login_page.dart
+++ capacitacao_flutter/lib/screens/login_page.dart
@@ -9,10 +9,34 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
+  late NetworkImage appear_password;
+  bool state_icon_password = true;
+
+  void initState(){
+    super.initState();
+    appear_password = NetworkImage("https://cdn-icons-png.flaticon.com/128/709/709612.png");
+  }
+
+   void changeIconPassword (){
+    setState(() { //o setState diz ao Flutter que o estado de alguma variável foi alterado e é necessário rederinzar tal estado novamente
+      if (state_icon_password == false){
+      appear_password = NetworkImage("https://cdn-icons-png.flaticon.com/128/709/709612.png");
+      state_icon_password = true;
+      print("Icon foi tocado");
+    }else{
+      appear_password = NetworkImage("https://cdn-icons-png.flaticon.com/128/2767/2767146.png");
+      state_icon_password = false;
+      print("Icon foi tocado");
+    }
+    });
+    
+  }
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+  
+ 
 
     return Scaffold(
       body: Stack(
@@ -23,7 +47,7 @@ class _LoginPage extends State<LoginPage> {
               width: screenWidth,
               height: 70*screenHeight/932,
               child: Image.asset("images/green_curve.png",
-              // fit: BoxFit.fill,
+              fit: BoxFit.cover,
               ),
             ),),
           Positioned( //Green points in the top of the screen
@@ -44,7 +68,7 @@ class _LoginPage extends State<LoginPage> {
               width: screenWidth,
               height: 70*screenHeight/932,
               child: Image.asset("images/red_curve.png",
-              // fit: BoxFit.fill,
+              fit: BoxFit.cover,
               ),
             ),),
           Positioned( //Red points in the back of the screen
@@ -56,7 +80,86 @@ class _LoginPage extends State<LoginPage> {
             left: 153*screenWidth/430,
             child: Text("Login", style: GoogleFonts.inter(
               fontSize: 48*screenHeight/932,
-              fontWeight: FontWeight.w400)))
+              fontWeight: FontWeight.w400))),
+          Stack(
+            children: [
+              Positioned(
+                top: 501*screenHeight/932,
+                left: 36*screenWidth/430,
+                child:Container(
+                height: screenHeight*58/932,
+                width: screenWidth*359/430,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(214, 221, 228, 1),
+                  borderRadius: BorderRadius.circular(40)
+                ),
+              )),
+              Positioned(
+                top:518*screenHeight/932,
+                left: 63*screenWidth/430,
+              child:Text("Usuário", 
+              style: GoogleFonts.inter(
+                fontSize: 20*screenHeight/932,
+                fontWeight: FontWeight.w400,
+              ),) )
+            ],
+          ),
+          Stack(
+            children: [
+              Positioned(
+                top: 581*screenHeight/932,
+                left: 36*screenWidth/430,
+                child:Container(
+                height: screenHeight*58/932,
+                width: screenWidth*359/430,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(214, 221, 228, 1),
+                  borderRadius: BorderRadius.circular(40)
+                ),
+              )),
+              Positioned(
+                top: 598*screenHeight/932,
+                left: 63*screenWidth/430,
+                child:Text("Senha", style: GoogleFonts.inter(
+                  fontSize: 20*screenHeight/932,
+                  fontWeight: FontWeight.w400
+                ),) ),
+              Positioned(
+                top:594*screenHeight/932,
+                right:58*screenWidth/430,
+                child: GestureDetector(
+                  onTap: () => changeIconPassword(),
+                  child:Image(image: appear_password, height: 31*screenHeight/932,width: 31*screenWidth/430,) ,
+                ))
+            ],
+          ),
+          Stack( //Enter button
+            children: [
+              Positioned(
+              top:672*screenHeight/932,
+              left:134*screenWidth/430,
+              child: Container(
+                height:screenHeight*58/932,
+                width: screenWidth*163/430,
+                decoration: BoxDecoration( //para estilizar container's no flutter usamos esse atributo "decoration" e a classe "BoxDecoration" 
+                //para alterar toda a estilização 
+                  borderRadius: BorderRadius.circular(40),
+                  color: Color.fromRGBO(0, 140, 69,1),
+                border: Border.all( //altera a estilização da borda do botão, aqui podemos estamos alterando a cor da borda, 
+                //mas podemos mudar outras coisas, basta apertar "ctrl + space" para ver as opções disponíveis
+                  color: Color.fromRGBO(0, 140, 69, 1)
+                )
+                ))),
+              Positioned(
+                top:686*screenHeight/932,
+                left:180*screenWidth/430,
+                child: Text("Entrar", style: GoogleFonts.inter(
+                  fontSize: screenHeight*24/932,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(244, 249, 255, 1)
+                ),))
+            ],
+          )
         ],
       ),
     );
